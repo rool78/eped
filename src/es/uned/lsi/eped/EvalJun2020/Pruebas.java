6bec5946d1fe87d2c3a01fdf5d836a444e6143a6
rool78 <raul.mazaa@gmail.com>
2020-05-27 20:51:29 +0200

    Tarea 2 Deque, StackDeque y QueueDeque

--- src/es/uned/lsi/eped/EvalJun2020/Pruebas.java
+++ src/es/uned/lsi/eped/EvalJun2020/Pruebas.java
@@ -1,23 +1,44 @@
 package es.uned.lsi.eped.EvalJun2020;
 
+import es.uned.lsi.eped.DataStructures.IteratorIF;
 import es.uned.lsi.eped.DataStructures.List;
 
 public class Pruebas {
 
 	public static void main(String[] args) {
-
-		List<Integer> l = new List<>();
+		Deque<Integer> q = new Deque<Integer>();
+		q.insertBack(5);
+		q.insertBack(6);
+		q.insertBack(7);
+		q.insertBack(8);
+		q.insertFront(1);
 		
-		l.insert(1, 1);
-		l.insert(2, 2);
-		l.insert(3, 3);
-		l.insert(2, 78);
 		
-		int i = 1;
-		while(i <= l.size()) {
-			System.out.println(l.get(i));
-			i++;
+		printStatus(q);
+		
+		System.out.println(q.getFront());
+		System.out.println(q.getBack());
+			
+		
+	}
+	
+	public static void printStatus(DequeIF<Integer> param) {
+		System.out.print("Tamaño: ");
+		System.out.println(param.size());
+		if(!param.isEmpty()){
+			System.out.print("Primer elemento: ");
+			System.out.println(param.getFront());
+			System.out.print("Último elemento: ");
+			System.out.println(param.getBack());
 		}
+		System.out.print("Iterador: ");
+		IteratorIF<Integer> it = param.iterator();
+		while (it.hasNext()) {
+			System.out.print(it.getNext());
+			if (it.hasNext()) { System.out.print(","); }
+		}
+		System.out.println();
+		System.out.println("--------------------------");
 	}
 
 }
